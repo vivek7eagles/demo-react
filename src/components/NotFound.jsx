afc96244488cc25b0f275dcef654aa66eb9583e2
viveksankar20 <viveksankar2010@gmail.com>
2025-01-30 09:37:19 +0530

    commited

--- src/components/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-function NotFound() {
-  return (
-    <div>
-      <h1 className="text-2xl text-center">404 not found this page</h1>
-    </div>
-  );
-}
-
-export default NotFound;
--- src/components/Welcome.jsx
+++
@@ -1,9 +0,0 @@
-function Welcome() {
-  return (
-    <div className="text-2xl font-bold text-center">
-      welcome to this web page
-    </div>
-  );
-}
-
-export default Welcome;
